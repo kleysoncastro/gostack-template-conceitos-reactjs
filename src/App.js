d459fbf6a1a396013c41bf045a4695d6238e4348
kleyson castro <kleysoncastro99@gmail.com>
2020-08-01 14:44:45 -0400

    Fix: factory metohd refresh stast repositories

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([])
+
+useEffect(()=>{
+  api.get('repositories').then(res =>{
+    setRepositories(res.data)
+  })
+},[])
+
   async function handleAddRepository() {
-    // TODO
+    
+    const tech = {
+      title: "github",
+      url: "https://github.com/kleysoncastro/gostack-template-conceitos-nodejs",
+      techs: "markdonw"
+    }
+
+    const response = await api.post('repositories', tech)
+
+    setRepositories([...repositories, response.data])
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    await api.delete(`repositories/${id}`)
+
+     setRepositories(repositories.filter(repo => repo.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(project =>(
 
-          <button onClick={() => handleRemoveRepository(1)}>
+  <li key={project.id} id="idStyle" >
+    
+    {project.title}
+    <button onClick={() => handleRemoveRepository(project.id)}>
             Remover
-          </button>
-        </li>
+    </button>
+  </li>
+         
+
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
